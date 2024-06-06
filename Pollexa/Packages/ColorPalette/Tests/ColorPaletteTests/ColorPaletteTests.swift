80e71fd070d900688110a83dbeb838f903befc3b
Adem Özsayın <ozsayinadem@gmail.com>
2024-06-06 19:31:22 +0300

    Add Color Pallete

---
+++ Pollexa/Packages/ColorPalette/Tests/ColorPaletteTests/ColorPaletteTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import ColorPalette
+
+final class ColorPaletteTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
